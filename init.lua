caad699087cedb060a73257fbd0712e95e2503b8
LEI <guillaume.frichet@gmail.com>
2018-08-24 12:25:55 +0200

    l

---
+++ init.lua
@@ -0,0 +1,8 @@
+function gorun()
+    local buf = CurView().Buf -- The current buffer
+    if buf:FileType() == "go" then
+        HandleShellCommand("go run " .. buf.Path, true, true) -- the first true means don't run it in the background
+    end
+end
+
+-- BindKey("CtrlR", "init.gorun")
